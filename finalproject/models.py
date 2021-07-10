59960494fabc6b1120cd6012b6bbd885c453d115
Unicornjavelin <86505375+Unicornjavelin@users.noreply.github.com>
2021-07-10 18:24:10 +0800

    18_24

--- finalproject/models.py
+++ finalproject/models.py
@@ -23,6 +23,7 @@ class Teacher(db.Model):
     __tablename__ = 'teacher'
     name = db.Column(db.String(45))
     id = db.Column(db.Integer, db.ForeignKey('user.uid'), primary_key=True)
+    courses = relationship('Course', backref='teacher')
 
     def __repr__(self):
         return '<teacher>{}:{}'.format(self.name, self.id)
@@ -168,13 +169,13 @@ class BEApplication(db.Model):  # student's application for by-election
     course_id = db.Column(db.Integer, db.ForeignKey("course.cid"))
 
 
-class Teacher_3(db.Model):
-    __bind_key__ = 'select_course'
-    __tablename__ = 'teacher'
-    __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(16), unique=True)
-    courses = relationship('Course', backref='teacher_3')
+# class Teacher_3(db.Model):
+#     __bind_key__ = 'select_course'
+#     __tablename__ = 'teacher'
+#     __table_args__ = {'extend_existing': True}
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(16), unique=True)
+#     courses = relationship('Course', backref='teacher_3')
 
 
 class Major(db.Model):
